7c299c308e281f2e1827daca512cbd7f83a38159
htootayzaaung <htootayzaaung.01@gmail.com>
2024-03-19 14:50:31 +0000

    Changes were made to wsgi.py

--- news_agency/news_agency/wsgi.py
+++ news_agency/news_agency/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/sc21hta/Web_Services_-_Web_Data/news_agency'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+# Ensure the 'DJANGO_SETTINGS_MODULE' is set to your project's settings
+os.environ['DJANGO_SETTINGS_MODULE'] = 'news_agency.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "news_agency.settings")
-
 application = get_wsgi_application()
